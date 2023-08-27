654a69f5a280a609184d4b23ff1b7c700a3879c3
MHDLamine <mouhamed.ngom50@gmail.com>
2023-08-27 15:51:29 +0200

    ok

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let r = document.getElementById("resultat");
 document.getElementById("egale").addEventListener("click", resultat);
-let valeur1 = "", valeur2 = "", signe = "";
+let valeur1 = "", valeur2 = "0", signe = "";
 
 
 function affectation (valeur) {
  if (signe == "") {
  
     valeur1 = valeur1 + valeur
-     console.log("one" + valeur1)
+    
  }
  if ((signe != "") && (valeur1 != "") ){
     valeur2 =  valeur2 + valeur;
-     console.log("two" +valeur2)
+  
  }
 
 }
@@ -19,36 +19,38 @@ function affectation (valeur) {
 
 function operation(s){
  signe = s
- console.log(signe);
+
 }
 function resultat () {
-    let valeur1Int = parseInt(valeur1);
-    let valeur2Int = parseInt(valeur2);
+    
+    let valeur1Float = parseFloat(valeur1);
+    let valeur2Float = parseFloat(valeur2);
     if (valeur2 != "") {
         if (signe == "+") {
-            r.innerHTML = valeur1Int + valeur2Int;
+            r.innerHTML = valeur1Float + valeur2Float;
+           
             
         }
         if (signe == "-") {
-            r.innerHTML = valeur1Int - valeur2Int;
+            r.innerHTML = valeur1Float - valeur2Float;
             
         }
         if (signe == "*") {
-            r.innerHTML = valeur1Int * valeur2Int;
+            r.innerHTML = valeur1Float * valeur2Float;
             
         }
 
         if (signe == "/") {
-            r.innerHTML = valeur1Int / valeur2Int;
+            r.innerHTML = valeur1Float / valeur2Float;
             
         }
         if (signe == "%") {
-            r.innerHTML = valeur1Int % valeur2Int;
+            r.innerHTML = valeur1Float % valeur2Float;
             
         }
         
     }
-  console.log(signe);
+
 }
 function reset() {
     valeur1 = "";
